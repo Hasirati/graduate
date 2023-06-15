f31a50cefbe88a276211dfbcb75a4188c96195c9
Ulia <yliia192@gmail.com>
2023-06-15 17:22:06 +0300

    section

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
+import printers from '../db.json'
 import Header from './Header/Header.js'
+import PrinterList from './PrinterList/PrinterList.js'
+import Section from './Section/Section.js'
 
 export default function App() {
 	return (
---
+++ src/components/Section/Section.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import css from './Section.module.css'
+
+function Section({ title, children }) {
+	return (
+		<div>
+			{title && <h2 className={css.h2}>{title}</h2>}
+			{children}
+		</div>
+	)
+}
+
+Section.prototype = {
+	title: PropTypes.string,
+}
+
+export default Section
+
+{
+	/* <Section title="Assortment">
+	<PrinterList items={printers} />
+</Section> */
+}
