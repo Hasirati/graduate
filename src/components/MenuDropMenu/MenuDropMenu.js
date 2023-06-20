5493bd3f8d624c29c346280236cb7a72c18fe85b
Ulia <yliia192@gmail.com>
2023-06-20 09:03:37 +0300

    name

--- src/components/MenuDropMenu/MenuDropMenu.js
+++ src/components/MenuDropMenu/MenuDropMenu.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import css from './MenuDropMenu.module.css'
-import Faza from './Faza'
+import MenuDrop from './MenuDrop'
 import { Link } from 'react-router-dom'
 
 export default function MenuDropMenu({ onClose }) {
 	return (
 		<div className={css.dropDownMenuDiv} onMouseLeave={onClose}>
 			<Link to='/payment'>
-				<Faza icon={'./assets/ikon/menuPayment.png'} name={'Payment'} />
+				<MenuDrop icon={'./assets/ikon/menuPayment.png'} name={'Payment'} />
 			</Link>
 			<Link to='/delivery'>
-				<Faza icon={'./assets/ikon/menuDelivery.png'} name={'Delivery'} />
+				<MenuDrop icon={'./assets/ikon/menuDelivery.png'} name={'Delivery'} />
 			</Link>
 			<Link to='/return'>
-				<Faza icon={'./assets/ikon/menuReturn.png'} name={'Return'} />
+				<MenuDrop icon={'./assets/ikon/menuReturn.png'} name={'Return'} />
 			</Link>
 			<Link to='/advices'>
-				<Faza icon={'./assets/ikon/menuAdvice.png'} name={'Advices'} />
+				<MenuDrop icon={'./assets/ikon/menuAdvice.png'} name={'Advices'} />
 			</Link>
 			<Link to='/contacts'>
-				<Faza icon={'./assets/ikon/menuContact.png'} name={'Contacts'} />
+				<MenuDrop icon={'./assets/ikon/menuContact.png'} name={'Contacts'} />
 			</Link>
 		</div>
 	)
